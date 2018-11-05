81db3a880918512a8b3cc6823127944dc3060cd1
Bjorn Hansen <bh@hoylu.com>
2018-11-04 22:57:39 -0800

    init cordova

--- timer-app/www/js/app.js
+++ timer-app/www/js/app.js
@@ -16,84 +16,88 @@ Vue.component('page-not-found', {
 });
 
 // Init App
-new Vue({
-  el: '#app',
-  data: function() {
-    return {
-      secondsRemaining: 60,
-      roundLength: 60,
-      interval: false,
-      ships: [
-        { color: 'green', isActive: true },
-        { color: 'blue', isActive: true },
-        { color: 'purple', isActive: true },
-        { color: 'red', isActive: true },
-        { color: 'orange', isActive: true },
-        { color: 'yellow', isActive: true }
-      ],
-      malfuctions: [
-        'has a fuel leak and loses one fuel',
-        'has dysentery and is unable to move',
-        "has broken it's tooling and must retool",
-      ],
-      // Framework7 parameters here
-      f7params: {
-        root: '#app', // App root element
-        id: 'io.framework7.testapp', // App bundle ID
-        name: 'Framework7', // App name
-        theme: 'auto', // Automatic theme detection
-        // App routes
-        routes: [
-          {
-            path: '/about/',
-            component: 'page-about'
-          },
-          {
-            path: '(.*)',
-            component: 'page-not-found'
-          }
-        ]
+function initApp() {
+  new Vue({
+    el: '#app',
+    data: function() {
+      return {
+        secondsRemaining: 60,
+        roundLength: 60,
+        interval: false,
+        ships: [
+          { color: 'green', isActive: true },
+          { color: 'blue', isActive: true },
+          { color: 'purple', isActive: true },
+          { color: 'red', isActive: true },
+          { color: 'orange', isActive: true },
+          { color: 'yellow', isActive: true }
+        ],
+        malfuctions: [
+          'has a fuel leak and loses one fuel',
+          'has dysentery and is unable to move',
+          "has broken its tooling and must retool"
+        ],
+        // Framework7 parameters here
+        f7params: {
+          root: '#app', // App root element
+          id: 'io.framework7.testapp', // App bundle ID
+          name: 'Framework7', // App name
+          theme: 'auto', // Automatic theme detection
+          // App routes
+          routes: [
+            {
+              path: '/about/',
+              component: 'page-about'
+            },
+            {
+              path: '(.*)',
+              component: 'page-not-found'
+            }
+          ]
+        }
+      };
+    },
+    computed: {
+      gaugeValue: function() {
+        return this.secondsRemaining / this.roundLength;
+      },
+      activeShips: function() {
+        return this.ships.filter(s => s.isActive);
+      },
+      isRunning: function() {
+        return !!this.interval;
       }
-    };
-  },
-  computed: {
-    gaugeValue: function() {
-      return this.secondsRemaining / this.roundLength;
     },
-    activeShips: function() {
-      return this.ships.filter(s => s.isActive);
-    },
-    isRunning: function() {
-      return !!this.interval;
-    }
-  },
-  methods: {
-    updateRoundLength: function(value) {
-      this.roundLength = value;
-      this.secondsRemaining = value;
-    },
-    toggleTimer: function() {
-      if (this.interval) {
-        clearInterval(this.interval);
-        this.interval = false;
-        this.secondsRemaining = this.roundLength;
-      } else {
-        this.interval = setInterval(() => {
-          this.secondsRemaining = this.secondsRemaining - 0.2;
-          if (this.secondsRemaining < 0 && this.secondsRemaining > -0.4) {
-            var ship = this.activeShips[
-              Math.floor(Math.random() * this.activeShips.length)
-            ];
-            var malfunction = this.malfuctions[
-              Math.floor(Math.random() * this.malfuctions.length)
-            ];
-            speak(`the ${ship.color} ship ${malfunction}`);
-          }
-        }, 200);
+    methods: {
+      updateRoundLength: function(value) {
+        this.roundLength = value;
+        this.secondsRemaining = value;
+      },
+      toggleTimer: function() {
+        if (this.interval) {
+          clearInterval(this.interval);
+          this.interval = false;
+          this.secondsRemaining = this.roundLength;
+        } else {
+          this.interval = setInterval(() => {
+            this.secondsRemaining = this.secondsRemaining - 0.2;
+            if (this.secondsRemaining < 0 && this.secondsRemaining > -0.4) {
+              var ship = this.activeShips[
+                Math.floor(Math.random() * this.activeShips.length)
+              ];
+              var malfunction = this.malfuctions[
+                Math.floor(Math.random() * this.malfuctions.length)
+              ];
+              speak(`the ${ship.color} ship ${malfunction}`);
+            }
+          }, 200);
+        }
       }
     }
-  }
-});
+  });
+}
+
+initApp();
 
 const synth = window.speechSynthesis;
 function speak(message) {
